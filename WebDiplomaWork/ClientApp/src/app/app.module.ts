57704df80b0bae0e92c0113fa8b0518fee35cc7a
AnnPkGit <103180183+AnnPkGit@users.noreply.github.com>
2023-04-30 16:28:56 +0300

    добавлены страницы регистрации и аутентификации

--- WebDiplomaWork/ClientApp/src/app/app.module.ts
+++ WebDiplomaWork/ClientApp/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { AuthComponent } from './identification/auth/auth.component';
+import { SignInComponent } from './identification/signIn/signIn.component';
+import { RegisterComponent } from './identification/register/register.component';
 
 @NgModule({
   declarations: [
@@ -16,16 +19,21 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    AuthComponent,
+    SignInComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: '', component: AuthComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'sign-in', component: SignInComponent },
+      { path: 'register', component: RegisterComponent },
     ])
   ],
   providers: [],
