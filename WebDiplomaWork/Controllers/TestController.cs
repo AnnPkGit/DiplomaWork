de11f37c838e7c2221ee80964f0e611db6e974cb
Alikhrik <siderdt@gmail.com>
2023-05-04 12:57:39 +0300

    Added Test classes

---
+++ WebDiplomaWork/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebDiplomaWork.Domain.Entities;
+using WebDiplomaWork.Infrastructure.DbAccess;
+using WebDiplomaWork.Infrastructure.DbAccess.SshAccess;
+
+namespace WebDiplomaWork.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly IRepository<TestEntity, string> _repository;
+
+        public TestController(IRepository<TestEntity, string> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IEnumerable<object> Get()
+        {
+            var tests = _repository.GetAll().ToList();
+            return tests;
+        }
+    }
+}
