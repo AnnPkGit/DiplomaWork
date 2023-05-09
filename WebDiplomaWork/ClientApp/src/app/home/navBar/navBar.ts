b3ba57803488b7a95969e8fadc1fb22029709d1d
AnnPkGit <103180183+AnnPkGit@users.noreply.github.com>
2023-05-09 09:17:30 +0300

    main page added

---
+++ WebDiplomaWork/ClientApp/src/app/home/navBar/navBar.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navBar',
+  templateUrl: './navBar.component.html',
+})
+export class NavBarComponent {
+}
---
+++ WebDiplomaWork/ClientApp/src/app/home/profileRec/recProfile.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rec-profile',
+  templateUrl: './recProfile.component.html',
+})
+export class RecProfileComponent {
+}
