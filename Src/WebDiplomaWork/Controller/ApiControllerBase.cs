2639c435d3c3f26924e670c9517fb1f5b54ee8bf
Alikhrik <siderdt@gmail.com>
2023-09-08 13:39:54 +0300

    Added the abstract Base controller

---
+++ Src/WebDiplomaWork/Controller/ApiControllerBase.cs
@@ -0,0 +1,15 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using WebDiplomaWork.Filters;
+
+namespace WebDiplomaWork.Controller;
+
+[ApiController]
+[ApiExceptionFilter]
+[Route("api/[controller]")]
+public abstract class ApiControllerBase : ControllerBase
+{
+    private ISender? _mediator;
+
+    protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<ISender>();
+}
