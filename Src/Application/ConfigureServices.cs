42720e6ee94b7c3435afa5964e91a82947010c65
Alikhrik <siderdt@gmail.com>
2023-09-11 15:29:10 +0300

    Added the FluentValidation library to the Application layer

--- Src/Application/ConfigureServices.cs
+++ Src/Application/ConfigureServices.cs
@@ -1,4 +1,7 @@
 using System.Reflection;
+using Application.Common.Behaviours;
+using FluentValidation;
+using MediatR;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
@@ -7,6 +10,7 @@ public static class ConfigureServices
     public static IServiceCollection AddApplicationServices(
         this IServiceCollection services)
     {
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         services.AddMediatR(cfg => {
             cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
         });
