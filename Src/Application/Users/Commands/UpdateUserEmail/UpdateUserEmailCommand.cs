db1230284e8972713f3511ba584361ed8b56d094
Alikhrik <siderdt@gmail.com>
2023-09-13 15:25:51 +0300

    Sending a test email message has been implemented

--- Src/Application/Users/Commands/UpdateUserEmail/UpdateUserEmailCommand.cs
+++ Src/Application/Users/Commands/UpdateUserEmail/UpdateUserEmailCommand.cs
@@ -11,13 +11,16 @@ public class UpdateUserEmailCommandHandler : IRequestHandler<UpdateUserEmailComm
 {
     private readonly IApplicationDbContext _context;
     private readonly ICurrentUserService _currentUserService;
+    private readonly IEmailService _emailService;
 
     public UpdateUserEmailCommandHandler(
         ICurrentUserService currentUserService,
-        IApplicationDbContext context)
+        IApplicationDbContext context,
+        IEmailService emailService)
     {
         _currentUserService = currentUserService;
         _context = context;
+        _emailService = emailService;
     }
 
     public async Task Handle(UpdateUserEmailCommand request, CancellationToken token)
@@ -28,6 +31,13 @@ public class UpdateUserEmailCommandHandler : IRequestHandler<UpdateUserEmailComm
         if (user == null)
             throw new NotFoundException(nameof(User), userId);
 
+        await _emailService.SendEmailAsync(new MailRequest
+        {
+            Subject = "no-reply",
+            ToEmail = request.NewEmail,
+            Body = "Test Message"
+        }, token);
+            
         user.Email = request.NewEmail;
         
         await _context.SaveChangesAsync(token);
