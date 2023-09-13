294b92b501fb4be21c4fb6e7edbf914905c47621
Alikhrik <siderdt@gmail.com>
2023-09-13 15:24:18 +0300

    Created EmailSettings configuration and changed config file

--- Src/Infrastructure/Configuration/ConfigurationManager/ConfigurationManager.cs
+++ Src/Infrastructure/Configuration/ConfigurationManager/ConfigurationManager.cs
@@ -4,10 +4,13 @@ namespace Infrastructure.Configuration.ConfigurationManager
 {
     public class ConfigurationManager : IConfigurationManager
     {
-        public DbConfiguration? DbConfiguration { get; }
+        public DbConfiguration DbConfiguration { get; }
+        public EmailSettings EmailSettings { get; }
+        
         public ConfigurationManager(IOptionsSnapshot<GeneralConfiguration> generalConfiguration)
         {
             DbConfiguration = generalConfiguration.Value.DbConfiguration;
+            EmailSettings = generalConfiguration.Value.EmailSettings;
         }
     }
 }
