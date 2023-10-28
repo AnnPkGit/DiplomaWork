d0a128091f184fb87564423d4add32803349aaff
AnnPkGit <103180183+AnnPkGit@users.noreply.github.com>
2023-10-28 13:50:42 +0200

    update

--- Src/WebDiplomaWork/ClientApp/src/app/home/toastCreator/toastCreator.component.ts
+++ Src/WebDiplomaWork/ClientApp/src/app/home/toastCreator/toastCreator.component.ts
@@ -1,11 +1,17 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { UserResponse } from 'src/app/identification/signIn/signIn.component';
 
 @Component({
   selector: 'app-toast-creator',
   templateUrl: './toastCreator.component.html',
 })
-export class ToastCreatorComponent {
-  
+export class ToastCreatorComponent implements OnInit{
+
+  public user: UserResponse | undefined;
+  ngOnInit(): void { 
+    this.user = JSON.parse(localStorage.getItem("userInfo") ?? "");
+  }
+
   autoGrow() {
     var textarea = document.getElementById('text-area');
     textarea = textarea ?? new HTMLElement();
