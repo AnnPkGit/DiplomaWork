780ab2fcfa1f38205a59bf44a898607e5b1b8a20
Alikhrik <siderdt@gmail.com>
2023-12-04 19:57:49 +0200

    Fixed bugs

--- Src/Application/Replies/Queries/GetRepliesByToast/GetRepliesByToastQuery.cs
+++ Src/Application/Replies/Queries/GetRepliesByToast/GetRepliesByToastQuery.cs
@@ -42,7 +42,7 @@ public class GetRepliesByToastQueryHandler : IRequestHandler<GetRepliesByToastQu
         var toastReplies = await _context.Replies
             .Where(r => r.ReplyToToastId == request.ToastWithContentId)
             .IgnoreAutoIncludes()
-            .Include(r => r.Author)
+            .Include(r => r.Author).ThenInclude(a => a.Avatar)
             .Include(r => r.Replies)
             .Include(r => r.Reactions)
             .Include(r => r.Quotes)
@@ -50,6 +50,7 @@ public class GetRepliesByToastQueryHandler : IRequestHandler<GetRepliesByToastQu
             .Include(r => r.MediaItems)
             .OrderByDescending(r => r.Created)
             .GetPaginatedSource(request.PageNumber, request.PageSize, out var totalCount)
+            .AsSingleQuery()
             .ToArrayAsync(cancellationToken);
 
         var toastRepliesDtos = toastReplies.Select(reply => _mapper.Map<ReplyBriefDto>(reply)).ToArray();
