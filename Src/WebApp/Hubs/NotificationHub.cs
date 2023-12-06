f98060d8c86da90bac38c338963e3e35d606462b
Alikhrik <siderdt@gmail.com>
2023-12-06 23:09:32 +0200

    Implemented the ViewNotifications command

--- Src/WebApp/Hubs/NotificationHub.cs
+++ Src/WebApp/Hubs/NotificationHub.cs
@@ -1,4 +1,5 @@
-﻿using Application.BaseNotifications.Queries.GetAllCurrentAccountNotifications;
+﻿using Application.BaseNotifications.Command.ViewNotifications;
+using Application.BaseNotifications.Queries.GetAllCurrentAccountNotifications;
 using Application.BaseNotifications.Queries.GetCurrentAccountNotificationsByTime;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
@@ -8,6 +9,16 @@ namespace WebApp.Hubs;
 [Authorize]
 public class NotificationHub : HubBase
 {
+    public async Task ViewNotifications(ViewNotificationsCommand command)
+    {
+        if (Mediator is not null)
+        {
+            await Mediator.Send(command);
+            await Clients.Caller.SendAsync("Receive", "OK");
+            return;
+        }
+        await Clients.Caller.SendAsync("Receive", null);
+    }
     public async Task Sync(GetCurrentAccountNotificationsByTimeQuery query)
     {
         if (Mediator is not null)
