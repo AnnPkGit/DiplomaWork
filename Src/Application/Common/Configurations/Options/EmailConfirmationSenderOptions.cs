3927645fd2056d69c4fa42c27fb2fd46c8cc76cb
Alikhrik <siderdt@gmail.com>
2023-12-07 14:06:58 +0200

    Added configuration infrastructure for EmailConfirmationSender

---
+++ Src/Application/Common/Configurations/Options/EmailConfirmationSenderOptions.cs
@@ -0,0 +1,16 @@
+﻿namespace Application.Common.Configurations.Options;
+
+public class EmailConfirmationSenderOptions
+{
+    public const string LINK_CONTENT = "#LinkContent";
+    public string ConfirmPageUrl { get; set; } = string.Empty;
+    public string LinkContent { get; set; } = string.Empty;
+    public string Body { get; set; } = string.Empty;
+
+    public bool CheckRequiredValues()
+    {
+        return ConfirmPageUrl != string.Empty &&
+               LinkContent != string.Empty &&
+               Body.Contains(LINK_CONTENT);
+    }
+}
